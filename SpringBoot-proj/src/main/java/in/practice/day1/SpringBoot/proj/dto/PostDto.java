67953faba75ad550f51b4525da8965affc987193
lahari178 <lahariraj8651@gmail.com>
2025-06-30 16:41:48 +0530

    mreged with main

--- SpringBoot-proj/src/main/java/in/practice/day1/SpringBoot/proj/dto/PostDto.java
+++ SpringBoot-proj/src/main/java/in/practice/day1/SpringBoot/proj/dto/PostDto.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class PostDto {
     private long id;
-    @Size(max = 2,message = "Size can't be more than 2 characters")
+    @Size(max = 3,message = "Size can't be more than 3 characters")
     private String title;
     @Size(min = 3,message = "Size can't be less than 3 characters")
     private String description;
